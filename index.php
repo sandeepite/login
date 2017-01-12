63f4c29acf6624fc9d519189664c155306474109
Sandeep Tiwari <sandeep100792@gmail.com>
2017-01-12 14:16:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ include_once 'session.php';
 <html>
     <head>
         <title>Home | Projects</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="theme-color" content="#0c84e4" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
